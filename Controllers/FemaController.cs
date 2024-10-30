5e3063b2f5d45545a184bf84c672bce3e2eb7072
HugoTheBugs <hugoferreira2323@gmail.com>
2024-10-29 21:02:48 -0300

    TESTE
    TESTE

---
+++ Controllers/FemaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEBCOREBCC2024.Controllers
+{
+    public class FemaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
