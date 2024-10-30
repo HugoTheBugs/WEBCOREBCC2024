78bc411e39b56ca661643e843ec821965d0cc7d7
HugoTheBugs <hugoferreira2323@gmail.com>
2024-10-29 21:07:29 -0300

    V1

--- obj/Debug/net8.0/WEBCOREBCC2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBCOREBCC2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBCOREBCC2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88c94d00dab48109006a3508d77d5d211aaaeff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e3063b2f5d45545a184bf84c672bce3e2eb7072")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBCOREBCC2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBCOREBCC2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
